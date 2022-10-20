2454f463f5fc91948aa90fa081ee7228a366d3c9
GARY <108233340+GARY121github@users.noreply.github.com>
2022-10-20 11:18:08 +0530

    Rotate Image

---
+++ src/Arrays/Two_D_Array/Rotate_Array_by_90_degrees.java
@@ -0,0 +1,36 @@
+package Arrays.Two_D_Array;
+
+import java.util.Scanner;
+
+/*
+
+*/
+public class Rotate_Array_by_90_degrees {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i= 0 ; i < n ; i++){
+            for(int j = 0 ; j < m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+//        String
+        rotate(arr);
+
+    }
+
+    static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
