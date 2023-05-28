11ab6abd06537711d61908ca93037d35936f4865
GARY <108233340+GARY121github@users.noreply.github.com>
2023-05-28 23:45:50 +0530

    TREE

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -147,12 +147,4 @@ public class BinaryTree {
 
 }
 
-class Node{
-    int data;
-    Node left;
-    Node right;
-    public Node(int data){
-        this.data = data;
-        this.left = this.right = null;
-    }
-}
+
