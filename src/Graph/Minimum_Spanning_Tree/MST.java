1169e77111eca6b58e6ba72d4a3a4f2bc03ae02e
GARY <108233340+GARY121github@users.noreply.github.com>
2023-12-08 22:28:21 +0530

    PRIMS ALGo for MST

--- src/Graph/Minimum_Spanning_Tree/MST.java
+++ src/Graph/Minimum_Spanning_Tree/MST.java
@@ -1,6 +1,7 @@
 package Graph.Minimum_Spanning_Tree;
 
 import Graph.Minimum_Spanning_Tree.Krushkals_Algorithm.Krushkals_Algo;
+import Graph.Minimum_Spanning_Tree.Prims_Algorithm.Prims_Algo;
 
 import java.util.Scanner;
 
@@ -8,15 +9,27 @@ public class MST {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Krushkals_Algo krushkals_algo = new Krushkals_Algo(n);
+//        Krushkals_Algo krushkals_algo = new Krushkals_Algo(n);
+//        int m = sc.nextInt();
+//        for (int i = 0 ; i < m ; i++){
+//            int v1 = sc.nextInt();
+//            int v2 = sc.nextInt();
+//            int cost = sc.nextInt();
+//            krushkals_algo.addEdge(v1 , v2 , cost);
+//        }
+//        int ans = krushkals_algo.KrushkalsAlgo();
+//        System.out.println(ans);
+
+        Prims_Algo prims_algo = new Prims_Algo(n);
         int m = sc.nextInt();
         for (int i = 0 ; i < m ; i++){
             int v1 = sc.nextInt();
             int v2 = sc.nextInt();
             int cost = sc.nextInt();
-            krushkals_algo.addEdge(v1 , v2 , cost);
+            prims_algo.addEdge(v1 , v2 , cost);
         }
-        int ans = krushkals_algo.KrushkalsAlgo();
+
+        int ans = prims_algo.PrimsAlgo();
         System.out.println(ans);
     }
 }
--- src/Graph/Minimum_Spanning_Tree/Prims_Algorithm/Prims_Algo.java
+++ src/Graph/Minimum_Spanning_Tree/Prims_Algorithm/Prims_Algo.java
@@ -1,4 +1,72 @@
 package Graph.Minimum_Spanning_Tree.Prims_Algorithm;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
 public class Prims_Algo {
+    private HashMap<Integer , HashMap<Integer , Integer>> map;
+
+    public Prims_Algo(int v){
+        this.map = new HashMap<>();
+
+        for (int i = 1 ; i <= v ; i++){
+            map.put(i , new HashMap<>());
+        }
+    }
+
+    public void addEdge(int v1 , int v2 , int cost){
+        this.map.get(v1).put(v2 , cost);
+        this.map.get(v2).put(v1 , cost);
+    }
+
+    private class EdgePair{
+        int v1;
+        int v2;
+        int cost;
+        public EdgePair(int v1 , int v2 , int cost){
+            this.v1 = v1;
+            this.v2 = v2;
+            this.cost = cost;
+        }
+    }
+
+    public int PrimsAlgo(){
+        PriorityQueue<EdgePair> minHeap = new PriorityQueue<>(new Comparator<EdgePair>() {
+            @Override
+            public int compare(EdgePair o1, EdgePair o2) {
+                return o1.cost - o2.cost;
+            }
+        });
+
+        HashSet<Integer> visited = new HashSet<>();
+        minHeap.add(new EdgePair(1 , 1 , 0));
+        int ans = 0;
+        while (!minHeap.isEmpty()){
+
+//            remove
+            EdgePair curr = minHeap.remove();
+
+//            ignore
+            if(visited.contains(curr.v1)){
+                continue;
+            }
+
+//            mark visited
+            visited.add(curr.v1);
+
+//            calculate
+            ans += curr.cost;
+
+            for (int nbrs : map.get(curr.v1).keySet()){
+                if(!visited.contains(nbrs)){
+                    EdgePair pair = new EdgePair(nbrs , curr.v1 , map.get(curr.v1).get(nbrs));
+                    minHeap.add(pair);
+                }
+            }
+        }
+
+        return ans;
+    }
 }
